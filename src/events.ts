8e6a79227ddc79d2ace29978d5fb9b35637e9999
Jake <baby@jakefox.de>
2026-08-07 20:16:26 +0200

    in events add the time remaning to the title

--- src/events.ts
+++ src/events.ts
@@ -45,13 +45,13 @@ function venueInfo(events: EurofurenceEvent[], showTitleWhenOpen = false) {
       globals.now.getTime() < new Date(event.end).getTime(),
   );
   if (current) {
-    if (showTitleWhenOpen) return current.title;
 
     let span = mergeSpans(accessible).find(
       (span) =>
         globals.now.getTime() >= span.start && globals.now.getTime() < span.end,
     );
-    return "open (" + datefuzzy(new Date(span.end)) + ")";
+    if (showTitleWhenOpen) return `${current.title} (${datefuzzy(new Date(span.end))})`;
+    return `open (${datefuzzy(new Date(span.end))})`;
   }
 
   let day_end = new Date(
